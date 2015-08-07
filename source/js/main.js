185543b9b611ae2ee1876f87867153314b4fb636
S1SYPHOS <schmardt@posteo.se>
2015-08-07 02:00:03 +0200

    Implemented day/night toggle

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,5 @@
-jQuery(document).ready(function($) {
-  var $inner_wrap = $('.inner-wrap');
-
-  $('.off-canvas-toggle__button').on('click', function(event) {
-    event.preventDefault();
-    $inner_wrap.toggleClass('is-open');
-  });
-
-  $('.off-canvas--close').on('click',function(event){
-    event.preventDefault();
-    $inner_wrap.removeClass('is-open');
+jQuery(document).ready(function() {
+  $('.toggle-input').change(function() {
+    $('body').toggleClass('night');
   });
 });
-
-// Insert your JS MAGIC HERE :)
